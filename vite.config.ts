057f252ac29f71bf5d84867d7a9f4cc37693dbbb
Anusha <anushakmd05@gmail.com>
2026-03-08 11:55:38 +0530

    fix base path for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        // --- ADDED THIS LINE FOR GITHUB PAGES DEPLOYMENT ---
-        base: "/Archive-It/", 
-        // --------------------------------------------------
+        base: "/",
         server: {
             port: 3000,
             host: '0.0.0.0',
